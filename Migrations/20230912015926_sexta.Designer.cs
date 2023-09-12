8401aad8e8c845f0228bcbdb0ba87ec664bf7e93
Guilherme <guilherme1984.araujo@gmail.com>
2023-09-11 23:04:41 -0300

    correção da model e view de seguros, feita uma nova migration

--- Migrations/20230912015926_sexta.Designer.cs
+++ Migrations/20230912015926_sexta.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniFrota.Context;
 
@@ -10,9 +11,11 @@ using MiniFrota.Context;
 namespace MiniFrota.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230912015926_sexta")]
+    partial class sexta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230912015926_sexta.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniFrota.Migrations
+{
+    /// <inheritdoc />
+    public partial class sexta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CNPJ",
+                table: "Seguros");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrecoCombustivel",
+                table: "Combustiveis",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CNPJ",
+                table: "Seguros",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrecoCombustivel",
+                table: "Combustiveis",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+    }
+}
